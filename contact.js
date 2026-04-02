5de5031564b79a1a98cb7fa4841ef929d1ff4ed1
Nat <cortn292@newschool.edu>
2026-04-02 17:52:12 -0400

    Fixing colors, making more anon before js

--- contact.js
+++ contact.js
@@ -1,2 +1,7 @@
+const records = document.querySelectorAll('.record');
+const slider = document.getElementById('record-slider');
+const sliderCur = document.getElementById('slider-cur');
+const sliderTotal = document.getElementById('slider-total');
+const recordId = document.getElementById('record-id');
+const dots = document.querySelectorAll('.dot');
 
- 
